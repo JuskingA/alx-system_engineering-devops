c0603da2cfd589824e5ac07005392b65b1e239bc
JuskingA <juskingadjeinyarko@gmail.com>
2023-01-11 05:50:07 +0000

    task2 update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# installing puppet link
-package { 'puppet-lint':
-    ensure   => '2.1.1',
-    provider => 'gem',
+# installing flask
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
