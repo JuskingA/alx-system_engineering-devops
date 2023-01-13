b07221afe7975555d5431a4d8722b0d78b6f0b2a
JuskingA <juskingadjeinyarko@gmail.com>
2023-01-13 15:05:10 +0000

    spa

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,12 +2,12 @@
 
 file_line { 'Turn off passwd auth':
   path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
+  line    => 'PasswordAuthentication no',
   replace => true
 }
 
 file_line { 'Declare identity file':
   path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/shool',
+  line    => 'IdentityFile ~/.ssh/shool',
   replace => true
 }
