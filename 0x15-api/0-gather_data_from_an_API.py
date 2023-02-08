396a6b452de5aaf5d2d8f9ff3b3336fa80f2ac29
JuskingA <juskingadjeinyarko@gmail.com>
2023-02-08 17:48:44 +0000

    Api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-""" With request ask for header"""
-import requests
-import sys
+""" Given employee ID, returns information about TODO list progress """
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        sys.exit(1)
-    else:
-        my_id = sys.argv[1]
-        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(my_id)
-        dict1 = requests.get(url).json()
-        name = dict1.get('name')
-        url2 = 'https://jsonplaceholder.typicode.com/users/{}/todos'.\
-               format(my_id)
-        list2 = requests.get(url2).json()
-        list_tasks = []
-        t_total = 0
-        t_done = 0
-        for tasks in list2:
-            if tasks.get('userId') == int(my_id):
-                t_total += 1
-                if tasks.get('completed'):
-                    list_tasks.append(tasks.get('title'))
-                    t_done += 1
-        print("Employee {} is done with tasks({}/{}):".format(name,
-                                                              t_done, t_total))
-        for tasks in list_tasks:
-            print("\t", tasks)
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    user = "https://jsonplaceholder.typicode.com/users/" + argv[1]
+    name = requests.get(user).json().get('name')
+    req = "https://jsonplaceholder.typicode.com/todos?userId=" + argv[1]
+    request = requests.get(req)
+
+    total = [task for task in request.json()]
+    completed = [task for task in request.json() if task.get('completed')]
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, len(completed), len(total)))
+    for task in completed:
+        print("\t {}".format(task.get('title')))
